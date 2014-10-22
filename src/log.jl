9cd46fb76dca0ad2d933b8152fa29ce531e342d2
Simon Byrne <simonbyrne@gmail.com>
2014-10-22 14:48:41 +0100

    inline, rearrange ops for log

--- src/log.jl
+++ src/log.jl
@@ -9,69 +9,6 @@
 
 
 # reduce to x = 2^k(1+f) where sqrt(2)/2 < 1+f < sqrt(2)
-function log_red(x::Float64)
-    s, k = frexp(x)
-    if s < 0.7071067811865476
-        s *= 2.0
-        k -= 1
-    end
-    k, s-1.0
-end
-
-# compute log of reduced quantity
-function log1p_kernel(k::Int,f::Float64)
-    ln2_hi = 6.93147180369123816490e-01
-    ln2_lo = 1.90821492927058770002e-10
-
-    hfsq = 0.5*f*f
-    s = f/(2.0+f)
-    z = s*s
-    R = z*@horner(z,
-                6.666666666666735130e-01,
-                3.999999999940941908e-01,
-                2.857142874366239149e-01,
-                2.222219843214978396e-01,
-                1.818357216161805012e-01,
-                1.531383769920937332e-01,
-                1.479819860511658591e-01)
-    k*ln2_hi+(f-(hfsq-(s*(hfsq+R)+(k*ln2_lo))))
-end
-
-function log1p_kernel_fma(k::Int,f::Float64)
-    ln2_hi = 6.93147180369123816490e-01
-    ln2_lo = 1.90821492927058770002e-10
-
-    hfsq = 0.5*f*f
-    s = f/(2.0+f)
-    z = s*s
-    R = z*@horner_fma(z,
-                6.666666666666735130e-01,
-                3.999999999940941908e-01,
-                2.857142874366239149e-01,
-                2.222219843214978396e-01,
-                1.818357216161805012e-01,
-                1.531383769920937332e-01,
-                1.479819860511658591e-01)
-    k*ln2_hi+(f-(hfsq-(s*(hfsq+R)+(k*ln2_lo))))
-end
-
-function log1p_kernel_muladd(k::Int,f::Float64)
-    ln2_hi = 6.93147180369123816490e-01
-    ln2_lo = 1.90821492927058770002e-10
-
-    hfsq = 0.5*f*f
-    s = f/(2.0+f)
-    z = s*s
-    R = z*@horner_muladd(z,
-                6.666666666666735130e-01,
-                3.999999999940941908e-01,
-                2.857142874366239149e-01,
-                2.222219843214978396e-01,
-                1.818357216161805012e-01,
-                1.531383769920937332e-01,
-                1.479819860511658591e-01)
-    k*ln2_hi+(f-(hfsq-(s*(hfsq+R)+(k*ln2_lo))))
-end
 
 # function
 function log(x::Float64)
@@ -79,8 +16,42 @@ function log(x::Float64)
         if isinf(x) 
             Inf
         else
-            k,f = log_red(x)
-            log1p_kernel(k,f)
+            xu = reinterpret(Uint64,x)
+            k = int(xu >> 52) & 0x07ff
+            if k == 0 # x is subnormal
+                x *= 0x1p54 # normalise significand
+                xu = reinterpret(Uint64,x)
+                k = int(xu >> 52) & 0x07ff - 54
+            end
+            k -= 1022
+            xu = (xu & 0x800f_ffff_ffff_ffff) | 0x3fe0_0000_0000_0000
+            s = reinterpret(Float64,xu)
+            if s < 0.7071067811865476
+                s *= 2.0
+                k -= 1
+            end
+            f = s-1.0
+            fk = float64(k)
+
+            ln2_hi = 6.93147180369123816490e-01
+            ln2_lo = 1.90821492927058770002e-10
+
+            lg1 = 6.666666666666735130e-01
+            lg2 = 3.999999999940941908e-01
+            lg3 = 2.857142874366239149e-01
+            lg4 = 2.222219843214978396e-01
+            lg5 = 1.818357216161805012e-01
+            lg6 = 1.531383769920937332e-01
+            lg7 = 1.479819860511658591e-01
+
+            hfsq = 0.5*f*f
+            s = f/(2.0+f)
+            z = s*s
+            w = z*z
+            t1 = w*@horner(w,lg2,lg4,lg6)
+            t2 = z*@horner(w,lg1,lg3,lg5,lg7)
+            R = t1+t2
+            fk*ln2_hi+(f-(hfsq-(s*(hfsq+R)+(fk*ln2_lo))))
         end
     elseif x == 0.0
         -Inf
@@ -89,13 +60,48 @@ function log(x::Float64)
     end
 end
         
+
 function log_fma(x::Float64)
     if x > 0.0
         if isinf(x) 
             Inf
         else
-            k,f = log_red(x)
-            log1p_kernel_fma(k,f)
+            xu = reinterpret(Uint64,x)
+            k = int(xu >> 52) & 0x07ff
+            if k == 0 # x is subnormal
+                x *= 0x1p54 # normalise significand
+                xu = reinterpret(Uint64,x)
+                k = int(xu >> 52) & 0x07ff - 54
+            end
+            k -= 1022
+            xu = (xu & 0x800f_ffff_ffff_ffff) | 0x3fe0_0000_0000_0000
+            s = reinterpret(Float64,xu)
+            if s < 0.7071067811865476
+                s *= 2.0
+                k -= 1
+            end
+            f = s-1.0
+            fk = float64(k)
+
+            ln2_hi = 6.93147180369123816490e-01
+            ln2_lo = 1.90821492927058770002e-10
+
+            lg1 = 6.666666666666735130e-01
+            lg2 = 3.999999999940941908e-01
+            lg3 = 2.857142874366239149e-01
+            lg4 = 2.222219843214978396e-01
+            lg5 = 1.818357216161805012e-01
+            lg6 = 1.531383769920937332e-01
+            lg7 = 1.479819860511658591e-01
+
+            hfsq = 0.5*f*f
+            s = f/(2.0+f)
+            z = s*s
+            w = z*z
+            t1 = w*@horner_fma(w,lg2,lg4,lg6)
+            t2 = @horner_fma(w,lg1,lg3,lg5,lg7)
+            R = fma(t2,z,t1)
+            fk*ln2_hi+(f-(hfsq-(s*(hfsq+R)+(fk*ln2_lo))))
         end
     elseif x == 0.0
         -Inf
@@ -109,8 +115,42 @@ function log_muladd(x::Float64)
         if isinf(x) 
             Inf
         else
-            k,f = log_red(x)
-            log1p_kernel_muladd(k,f)
+            xu = reinterpret(Uint64,x)
+            k = int(xu >> 52) & 0x07ff
+            if k == 0 # x is subnormal
+                x *= 0x1p54 # normalise significand
+                xu = reinterpret(Uint64,x)
+                k = int(xu >> 52) & 0x07ff - 54
+            end
+            k -= 1022
+            xu = (xu & 0x800f_ffff_ffff_ffff) | 0x3fe0_0000_0000_0000
+            s = reinterpret(Float64,xu)
+            if s < 0.7071067811865476
+                s *= 2.0
+                k -= 1
+            end
+            f = s-1.0
+            fk = float64(k)
+
+            ln2_hi = 6.93147180369123816490e-01
+            ln2_lo = 1.90821492927058770002e-10
+
+            lg1 = 6.666666666666735130e-01
+            lg2 = 3.999999999940941908e-01
+            lg3 = 2.857142874366239149e-01
+            lg4 = 2.222219843214978396e-01
+            lg5 = 1.818357216161805012e-01
+            lg6 = 1.531383769920937332e-01
+            lg7 = 1.479819860511658591e-01
+
+            hfsq = 0.5*f*f
+            s = f/(2.0+f)
+            z = s*s
+            w = z*z
+            t1 = w*@horner_muladd(w,lg2,lg4,lg6)
+            t2 = @horner_muladd(w,lg1,lg3,lg5,lg7)
+            R = muladd(t2,z,t1)
+            fk*ln2_hi+(f-(hfsq-(s*(hfsq+R)+(fk*ln2_lo))))
         end
     elseif x == 0.0
         -Inf
@@ -118,3 +158,4 @@ function log_muladd(x::Float64)
         NaN
     end
 end
+
