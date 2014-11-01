85f5ec7d1548dffd8c158b004c245cf1bac53757
Simon Byrne <simonbyrne@gmail.com>
2014-11-01 14:46:34 +0000

    fix reference

--- src/log_tang.jl
+++ src/log_tang.jl
@@ -1,7 +1,8 @@
-# "Table-driven Implementation of the Exponential Function in IEEE Floating-point Arithmetic"
-# Tang, Ping-Tak Peter
-# ACM Trans. Math. Softw., 1989, 15(2):144--157
-# http://doi.acm.org/10.1145/63522.214389
+# Implementation of
+#  "Table-driven Implementation of the Logarithm Function in IEEE Floating-point Arithmetic"
+#  Tang, Ping-Tak Peter
+#  ACM Trans. Math. Softw. (1990), 16(4):378--400
+#  http://dx.doi.org/10.1145/98267.98294
 
 # implemented in
 #  Apple OS X libm (APSL v2)
